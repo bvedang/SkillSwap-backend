929825d1a6ab45c935197f701a60a5d3402411f7
Vedang Barhate <dvedang4u@gmail.com>
2023-06-28 16:05:43 -0500

    Configured Init.py file in the app directory

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-print("Lets Learn something new!")
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, JWTManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "mysecret"
+app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+cors = CORS(app)
+jwt = JWTManager(app)
